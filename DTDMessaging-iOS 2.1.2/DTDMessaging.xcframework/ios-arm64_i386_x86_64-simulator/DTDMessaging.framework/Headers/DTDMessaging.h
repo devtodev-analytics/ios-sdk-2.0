9aec2a3b1de629305b441c366a410072d4d7b58a
Антон Васильченко <Blakend2@gmail.com>
2022-03-30 12:03:53 +0500

    Release 2.1.2

---
+++ DTDMessaging-iOS 2.1.2/DTDMessaging.xcframework/ios-arm64_i386_x86_64-simulator/DTDMessaging.framework/Headers/DTDMessaging.h
@@ -0,0 +1,13 @@
+//
+//  Copyright © devtodev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for messaging.
+FOUNDATION_EXPORT double messagingVersionNumber;
+
+//! Project version string for messaging.
+FOUNDATION_EXPORT const unsigned char messagingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <messaging/PublicHeader.h>
--- DTDMessaging.podspec
+++ DTDMessaging.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "DTDMessaging"
-  s.version      = "2.1.1"
+  s.version      = "2.1.2"
   s.summary      = "DTDMessaging for iOS"
   s.description  = <<-DESC
   DTDMessaging add-on for the DTDAnalytics platform allows you to work with push notifications.
